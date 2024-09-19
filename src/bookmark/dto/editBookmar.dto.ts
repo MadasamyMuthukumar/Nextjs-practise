f590bf321044083504e97f6859c0840e7f13578d
Muthu <madasamy939@gmail.com>
2024-09-19 18:30:24 +0530

    Bookmark Dtos

---
+++ src/bookmark/dto/editBookmar.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator"
+
+
+export class EditBookmarkDto{
+    @IsString()
+    @IsOptional()
+    title?:string
+
+    @IsString()
+    @IsOptional()
+    description?:string
+
+    @IsString()
+    @IsOptional()
+    link?:string
+}
