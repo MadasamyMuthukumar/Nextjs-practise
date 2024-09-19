e5f45952b79e88130e856e069d56dc0518754f76
Muthu <madasamy939@gmail.com>
2024-09-19 18:28:31 +0530

    Module configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,33 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServiceController } from './service/service.controller';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal:true //making cofigService globally available 
   }), //this will add our env file to all our application
-     AuthModule, UserModule, BookmarkModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+     AuthModule, UserModule, BookmarkModule, PrismaModule,
+     //we are defining two limits here short & long
+    ThrottlerModule.forRoot([
+      {
+        name:'long',
+      ttl:60000,
+      limit:3  //limiting 3 request per minute
+    },
+    {
+      name:'short',
+      ttl:1000,
+      limit:2
+    }
+  ])], 
+  controllers: [AppController, ServiceController],
+
+  providers: [AppService,{
+    provide:APP_GUARD,   //providing rate limiting to all our controller routes
+    useClass:ThrottlerGuard
+  }],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,38 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true  //will stirp out the data from req that are not defined in dto
   }))
+  // app.setGlobalPrefix('/api') // /api will be the global prefix for all apis
+  app.enableCors() //everyone can access this api
+
+  // app.enableCors({
+  //   origin: 'https://example.com', // Only allow this origin
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+  //   allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+  //   credentials: true, // Allow credentials
+  // });
   await app.listen(3000);
 }
 bootstrap();
+
+
+/**
+ *  dynamically allowing origins based on incoming requests, 
+ * you can use a function for the origin option. 
+ * This function receives the request's origin and can conditionally allow or deny it.
+ */
+
+// app.enableCors({
+//   origin: (origin, callback) => {
+//     const allowedOrigins = ['https://example1.com', 'https://example2.com', 'https://example3.com'];
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       // Allow the request if the origin is in the allowed list or if there's no origin (e.g., in server-to-server requests)
+//       callback(null, true);
+//     } else {
+//       // Deny the request
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   allowedHeaders: 'Content-Type, Authorization',
+//   credentials: true,
+// });
